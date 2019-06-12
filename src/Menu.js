0ab462c40c2108d15634046f6a7fb5b0e512e8f4
sajjal <nairsajjal@gmail.com>
2019-06-12 15:54:06 +0530

    test commit 1

---
+++ src/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Menu = () => {
+    
+    return (
+
+        <div className="Menustyle">
+            <ul>
+
+                <li>Home </li>
+                <li>About</li>
+                <li>Contact </li>
+            </ul>
+        </div>
+    )
+};
+
+export default Menu
