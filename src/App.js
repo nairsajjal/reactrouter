9924a2edb1af819e007684b0d1203c55a5da7530
sajjal <nairsajjal@gmail.com>
2019-06-12 16:34:05 +0530

    routed commit 1

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import './App.css';
 import Menu from './Menu';
 import About from './About';
 import Contact from './Contact';
+import {BrowserRouter, Route} from 'react-router-dom';
+
 function App() {
   return (
+    <BrowserRouter>
     <div >
         <Menu />
         <Home />
         <About />
+        <Route path="/about" component={About} />
+        <Route path="/home" component={Home} />
+        <Route path="/contact" component={Contact} />
         <Contact />
     </div>
+    </BrowserRouter>
   );
 }
 const Home = () =>{
